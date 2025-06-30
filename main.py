a8859d46a06b6be1896f6d25179d1030757b1774
Pniabzyk <pniabzyk@gmail.com>
2025-06-30 14:27:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,3 +90,7 @@ def wfp_webhook():
 @app.route('/', methods=['GET'])
 def index():
     return 'PniabzykBot is running.'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
